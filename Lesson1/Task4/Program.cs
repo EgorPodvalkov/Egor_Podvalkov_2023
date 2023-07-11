2c94c9b433332b7cbf83a400dc6fed90887e319a
EgorPodvalkov <egorpodvalkov@gmail.com>
2023-07-11 20:42:48 +0300

    Added Lesson1/Task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Getting first string
+Console.Write("Enter 1 string: ");
+var str1 = Console.ReadLine();
+
+// Getting second string
+Console.Write("Enter 2 string: ");
+var str2 = Console.ReadLine();
+
+// If first longer
+if (str1.Length > str2.Length)
+    Console.WriteLine($"Result: {str1 + str2}");
+// If second longer
+else if (str1.Length < str2.Length)
+{
+    // Splitting
+    var splitter = str1.Length > 0 ? str1[0] : ' ';
+    var result = str2.Split(splitter);
+    Console.WriteLine("Result: ");
+
+    // Returning result
+    foreach (var str in result)
+    {
+        Console.WriteLine(str);
+    }
+}
+// If equal length
+else
+    Console.WriteLine("Result: ");
